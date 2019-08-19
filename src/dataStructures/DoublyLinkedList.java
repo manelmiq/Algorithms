77318ccb06da4552788559611db7955115ac90e9
Emmanuell <emmanuellnogmiq@hotmail.com>
2019-08-19 01:25:48 -0500

    doublyLInked List

---
+++ src/dataStructures/DoublyLinkedList.java
@@ -0,0 +1,85 @@
+package dataStructures;
+
+public class DoublyLinkedList {
+
+    Node head;
+    Node tail;
+    int size = 0;
+
+    class Node {
+        String val;
+        Node next;
+        Node previous;
+
+        Node(String val) {
+            this.val = val;
+        }
+
+        public Node(String val, Node next, Node previous) {
+            this.val = val;
+            this.next = next;
+            this.previous = previous;
+        }
+    }
+
+    public void addFirst(String element) {
+        Node newNode = new Node(element, head, null);
+        if (head != null) {
+            head.previous = newNode;
+        }
+        head = newNode;
+        if (tail == null) {
+            tail = newNode;
+        }
+        size++;
+        System.out.println("adding: " + element);
+    }
+
+
+    public void addLast(String element) {
+        Node newNode = new Node(element, null, tail);
+        if (tail != null) {
+            tail.next = newNode;
+        }
+        tail = newNode;
+        if (head == null) {
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void removeByValue(String element) {
+        Node tmp = head;
+        Node begin = new Node("", null, null);
+        while (tmp != null) {
+            if (tmp.val.equals(element)) {
+                begin.next = tmp.next;
+                if (tmp.next != null) {
+                    tmp.next.previous = begin;
+                }
+                head = begin;
+                break;
+            }
+            Node newNode = new Node(tmp.val, null, begin);
+            begin = newNode;
+            tmp = tmp.next;
+        }
+    }
+
+    @Override
+    public String toString() {
+        Node tmp = head;
+        StringBuilder str = new StringBuilder();
+        int count = 0;
+        while (tmp != null) {
+            str.append( (count > 0) ? ", " + tmp.val :  tmp.val);
+            count++;
+            tmp = tmp.next;
+        }
+        return str.toString();
+    }
+
+    public DoublyLinkedList() {
+        this.size = 0;
+    }
+}
---
+++ src/dataStructures/TestLinkedList.java
@@ -0,0 +1,42 @@
+package dataStructures;
+
+
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+
+public class TestLinkedList  {
+
+    @Test
+    public void  testAdd(){
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.addFirst("john");
+        list.addFirst("mary");
+        assertEquals(list.size , 2);
+    }
+
+    @Test
+    public void  testInsertOrder(){
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.addFirst("mary");
+        list.addFirst("john");
+        assertEquals(list.toString() , "mary, john");
+    }
+    @Test
+    public void  testInsertLast(){
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.addLast("mary");
+        list.addLast("john");
+        assertEquals(list.toString() , "mary, john");
+    }
+    @Test
+    public void  testRemoveByValue(){
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.addFirst("mary");
+        list.addFirst("john");
+        list.addFirst("natalie");
+        list.addFirst("joseph");
+        list.removeByValue("natalie");
+        assertEquals(list.toString() , "joseph, john, mary");
+    }
+}
